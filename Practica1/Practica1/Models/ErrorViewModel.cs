b846c51b3eeff94062ac7f6d479b883ceab2d909
Alejrr2 <alejhandrorr@gmail.com>
2024-09-27 11:24:51 -0600

    Limpieza

--- Practica1/Practica1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Practica1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
